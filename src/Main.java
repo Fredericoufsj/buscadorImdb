524a131a8700834c2a45ebd4dbcc81deec5d20e1
Frederico Thomaz <frederico.santos@supersim.com.br>
2023-06-28 09:23:00 -0300

    parse das urls da imagem

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,27 @@ import models.TituloImdb;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("##### Buscador de Filmes IMDB #######");
         BuscaFilmes filmes = new BuscaFilmes();
         String filmesJson = filmes.response.body();
-        //Gson gson = new Gson().newBuilder().setPrettyPrinting().create();
 
-        FileWriter escreve = new FileWriter("filmes.json");
-        escreve.write(filmesJson);
-        escreve.close();
+        // Busca no filmesJson pela url da imagem
+        Pattern patternInicioUrlImagem = Pattern.compile("https");
+        Pattern patternFimUrlImagem = Pattern.compile("jpg");
+        Matcher matcherInicioUrlImagem = patternInicioUrlImagem.matcher(filmesJson);
+        Matcher matcherFimUrlImagem = patternFimUrlImagem.matcher(filmesJson);
+
+        List<String> imagens = new ArrayList<>();
+        while (matcherInicioUrlImagem.find() && matcherFimUrlImagem.find()) {
+            String urlImagem = filmesJson.substring(matcherInicioUrlImagem.start() + 3, matcherFimUrlImagem.start() - 3);
+            imagens.add(urlImagem);
+           // System.out.println(urlImagem);
+        }
     }
 }
