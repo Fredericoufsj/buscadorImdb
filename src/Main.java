e8499e4a8b5718e535db00d128b44ad6509c87a4
Frederico Thomaz <frederico.santos@supersim.com.br>
2023-07-03 21:43:41 -0300

    busca pela nota

--- src/Main.java
+++ src/Main.java
@@ -25,30 +25,37 @@ public class Main {
         while (matcherUrlImagem.find()) {
             String urlImagem = matcherUrlImagem.group();
             imagens.add(urlImagem);
-            // System.out.println(urlImagem);
+       //     System.out.println(urlImagem);
         }
 
-
-// Busca no filmesJson pelo ano
+        // Busca no filmesJson pelo ano
         Pattern patternAno = Pattern.compile("\"year\":\\s*\"(\\d{4})\"");
         Matcher matcherAno = patternAno.matcher(filmesJson);
         List<String> filmeAnos = new ArrayList<>();
         while (matcherAno.find()) {
             String ano = matcherAno.group(1);
             filmeAnos.add(ano);
-         //   System.out.println(ano);
+       //     System.out.println(ano);
         }
 
-
-
-// Busca no filmesJson pelo título
+        // Busca no filmesJson pelo título
         Pattern patternTitulo = Pattern.compile("\"title\":\\s*\"([^\"]*)\"");
         Matcher matcherTitulo = patternTitulo.matcher(filmesJson);
         List<String> titles = new ArrayList<>();
         while (matcherTitulo.find()) {
             String titulo = matcherTitulo.group(1);
             titles.add(titulo);
-            System.out.println(titulo);
+         //   System.out.println(titulo);
+        }
+
+        // Busca no filmesJson pela nota
+        Pattern patternNota = Pattern.compile("\"imDbRating\":\\s*\"([^\"]+)\"");
+        Matcher matcherNota = patternNota.matcher(filmesJson);
+        List<String> notas = new ArrayList<>();
+        while (matcherNota.find()) {
+            String nota = matcherNota.group(1);
+            notas.add(nota);
+         //   System.out.println(nota);
         }
     }
 }
